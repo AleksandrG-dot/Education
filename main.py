2c8f614eff415c72fa8a1fce72c97a8a3383faa1
Aleksandr Goncharov <potta4edu@gmail.com>
2024-07-03 01:46:34 +0500

    commit after linters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src import masks, widget
+from src import widget
+
 # from src import widget
 
 # Задача 1 в модуле 2
@@ -6,15 +7,17 @@ from src import masks, widget
 # print(masks.get_mask_account(73654108430135874305))
 
 # Задача 2 в модуле 2
-inp_card_or_account = ['Visa Platinum 7000 7922 8960 6361',
-                       'Maestro 1596837868705199',
-                       'Счет 64686473678894779589',
-                       'MasterCard 7158300734726758',
-                       'Счет 35383033474447895560',
-                       'Visa Classic 6831982476737658',
-                       'Visa Platinum 8990922113665229',
-                       'Visa Gold 5999414228426353',
-                       'Счет 73654108430135874305']
+inp_card_or_account = [
+    "Visa Platinum 7000 7922 8960 6361",
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305",
+]
 
 for i in inp_card_or_account:
     print(widget.mask_account_card(i))
--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,26 @@
 """ Функции виджета """
+
 from src import masks
 
+
 def mask_account_card(card_or_account: str) -> str:
-    """ Функция возвращает маскированный номер карты или счета с произвольным входом сохраняя фифференциацию """
-    payment_system = ('visa', 'maestro', 'mastercard')  # Поддерживаемые платежные системы
+    """Функция возвращает маскированный номер карты или счета с произвольным входом сохраняя фифференциацию"""
+    payment_system = ("visa", "maestro", "mastercard")  # Поддерживаемые платежные системы
     tmp_list: list[str] = card_or_account.split()
-    if tmp_list[0].lower() == 'счет':
-        return 'Счет ' + masks.get_mask_account(int(tmp_list[1]))
+    if tmp_list[0].lower() == "счет":
+        return "Счет " + masks.get_mask_account(int(tmp_list[1]))
     elif tmp_list[0].lower() in payment_system:
         tmp_paysistem = f"{tmp_list[0]} {tmp_list[1] + ' ' if tmp_list[1].isalpha() else ''}"
-        if len(tmp_list) > 3:                                   # Если номер карты разбит по 4 цифры
-            tmp_card_number = int(''.join(tmp_list[-4:]))
-        else:                                                   # Если номер карты передан единым числом
-            tmp_card_number = int(''.join(tmp_list[-1]))
+        if len(tmp_list) > 3:  # Если номер карты разбит по 4 цифры
+            tmp_card_number = int("".join(tmp_list[-4:]))
+        else:  # Если номер карты передан единым числом
+            tmp_card_number = int("".join(tmp_list[-1]))
         return f"{tmp_paysistem} {masks.get_mask_card_number(tmp_card_number)}"
     else:
-        return 'incorrect data'
+        return "incorrect data"
+
 
 def get_date(inp_date: str) -> str:
     """Конвертор времени из ISO 8601 в ДД.ММ.ГГГГ"""
-    tmp_date = inp_date.split('T')[0].split('-')
-    return '.'.join(tmp_date[::-1])
+    tmp_date = inp_date.split("T")[0].split("-")
+    return ".".join(tmp_date[::-1])
