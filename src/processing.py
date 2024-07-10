c12683ae5c02832cedcfe734505c95ec5151738e
Aleksandr Goncharov <potta4edu@gmail.com>
2024-07-11 01:54:01 +0500

    редактирование widget для соответствия тестам

--- src/processing.py
+++ src/processing.py
@@ -9,4 +9,4 @@ def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict
 
 def sort_by_date(list_dict: list[dict], reverse: bool = True) -> list[dict]:
     """Функция сортирует транзакции по дате выполнения, по умолчанию - убывание"""
-    return sorted(list_dict, key=lambda x: x["date"], reverse= reverse)
+    return sorted(list_dict, key=lambda x: x["date"], reverse=reverse)
