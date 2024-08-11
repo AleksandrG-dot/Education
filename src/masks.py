263a4c7860d2c0582f0079d788e12ee89fd6efe4
Aleksandr Goncharov <potta@inbox.ru>
2024-08-11 14:38:57 +0500

    add logging for module masks utils

--- src/masks.py
+++ src/masks.py
@@ -1,36 +1,37 @@
 """ Содержит функции, маскирующие номера карт и счетов. """
+
 import logging
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler('logs/masks.log')
-file_formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s: %(message)s')
+file_handler = logging.FileHandler("logs/masks.log", "w")
+file_formatter = logging.Formatter("%(asctime)s-%(name)s-%(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(card_number: int) -> str:
     """Функция маскировки номера карты."""
-    logger.info('Запуск функции маскировки нормера карты get_mask_card_number')
+    logger.info("Запуск функции маскировки нормера карты get_mask_card_number")
     if type(card_number) is not int:
-        logger.error('В функцию get_mask_card_number передано не число')
+        logger.error(f"В функцию get_mask_card_number передано не число: {card_number}")
         raise TypeError("Type error. Please use int type.")
     if card_number < 1000000000000000 or card_number > 9999999999999999:
-        logger.error(f'В функцию get_mask_card_number передано число содержащее не 16 цифр: {card_number}')
+        logger.error(f"В функцию get_mask_card_number передано число содержащее не 16 цифр: {card_number}")
         raise TypeError("Invalid number of digits. Must be 16 digits.")
     tmp_str = str(card_number)
-    logger.info('Завершение работы функции маскировки номера карты get_mask_card_number')
+    logger.info("Завершение работы функции маскировки номера карты get_mask_card_number")
     return f"{tmp_str[0:4]} {tmp_str[4:6]}** **** {tmp_str[12:]}"
 
 
 def get_mask_account(account_number: int) -> str:
     """Функция маскировки номера счета."""
-    logger.info('Запуск функции маскировки нормера счета get_mask_account')
+    logger.info("Запуск функции маскировки нормера счета get_mask_account")
     if type(account_number) is not int:
-        logger.error('В функцию get_mask_account передано не число')
+        logger.error(f"В функцию get_mask_account передано не число: {account_number}")
         raise TypeError("Type error. Please use int type.")
     if account_number < 10000000000000000000 or account_number > 99999999999999999999:
-        logger.error(f'В функцию get_mask_account передано число содержащее не 20 цифр: {account_number}')
+        logger.error(f"В функцию get_mask_account передано число содержащее не 20 цифр: {account_number}")
         raise TypeError("Invalid number of digits. Must be 20 digits.")
-    logger.info('Завершение работы функции маскировки номера счета get_mask_account')
+    logger.info("Завершение работы функции маскировки номера счета get_mask_account")
     return f"**{str(account_number)[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,48 @@
 import json
+import logging
 
 from src import external_api
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("logs/utils.log", "w")
+file_formatter = logging.Formatter("%(asctime)s-%(name)s-%(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def downloading_financial_transaction_data(path: str) -> list[dict]:
     """Функция читает из JSON-файла данные о финансовых транзакция и возвращает список словарей"""
+    logger.info(f"Запуск функции чтения JSON-файла downloading_financial_transaction_data({path})")
     try:
         with open(path, encoding="utf-8") as f:
             data = json.load(f)
-    except Exception:
+    except Exception as e:
+        logger.error(f"Функция downloading_financial_transaction_data: Произошла ОШИБКА: {e}", exc_info=True)
         data = []
+    logger.info("Завершение работы функции чтения JSON-файла downloading_financial_transaction_data")
     return data
 
 
 def get_amount(transaction: dict) -> float:
     """Возвращает сумму транзакции в рублях. Если исходная транзакция не в рублях, то переводит
     в рубли по текущему курсу. На вход получает тразакцию."""
-    result = transaction["operationAmount"]["amount"]
-    currency = transaction["operationAmount"]["currency"]["code"]
+    logger.info("Запуск функции возвращающей сумму транзацкии get_amount")
+    try:
+        result = transaction["operationAmount"]["amount"]
+        currency = transaction["operationAmount"]["currency"]["code"]
+    except Exception as e:
+        logger.error(f"Функция get_amount: Произошла ОШИБКА: {e}", exc_info=True)
+        raise e
     if currency != "RUB":
-        result = external_api.conversion_in_rub(result, currency)
+        logger.info("Функция get_amount: Запрос котировок через API")
+        try:
+            result = external_api.conversion_in_rub(result, currency)
+        except Exception as e:
+            logger.error(
+                f"Ф. get_amount: external_api.conversion_in_rub({result}, {currency}): Произошла ОШИБКА: {e}",
+                exc_info=True,
+            )
+            raise e
+    logger.info("Завершение работы функции возвращающей сумму транзацкии get_amount")
     return float(result)
